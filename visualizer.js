54df3ade6548ed0c605d009c7489668588fc57d7
Shuichi Aizawa <sa396@columbia.edu>
2015-10-08 11:51:24 -0400

    Visualizer point cloud

--- visualizer.js
+++ visualizer.js
@@ -4,8 +4,8 @@ function Visualizer(canvas) {
 	var gl = twgl.getWebGLContext(canvas);
 	var programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
 
-	var data = new Uint8Array(1024);	// set to frequencyBinCount, half of fftSize
-	var length = Math.ceil(data.length * 0.73);	// frequencies are mostly flat towards highs
+	var length = 744;	// analyser.frequencyBinCount - trim off the high end which are flat anyway
+	var data = new Uint8Array(length);
 	var positions = new Float32Array(length * 2);
 	for (var i = length-1; i >= 0; --i) {
 		positions[i*2] = i / length * 2 - 1;	// x normalized to -1 ~ 1
@@ -25,6 +25,6 @@ function Visualizer(canvas) {
 		gl.useProgram(programInfo.program);
 		twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
 		twgl.setUniforms(programInfo, uniforms);
-		twgl.drawBufferInfo(gl, gl.LINE_STRIP, bufferInfo);
+		twgl.drawBufferInfo(gl, gl.POINT, bufferInfo);
 	}
 }
