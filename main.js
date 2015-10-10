241201b6b3e7ae608c77b712f3357fa0450e111d
Shuichi Aizawa <sa396@columbia.edu>
2015-10-10 15:07:06 -0400

    Mirage/Aurora shader improvements.

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 (function(){
 var canvas, context2d, audioContext, visualizer, vars={}, styles=[], tracks=[], logs=[];
 var colors = [
-	[  0,  0,  1],
 	[  1,  0,  0],
+	[  0,  0,  1],
 	[  0,0.5,  0],
 	[  1,0.5,  0],
 	[  0,  0,0.5],
